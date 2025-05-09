5b330a7c8239238c3d5687820fb9cc4f013d1c5b
Quinchy <deguzmancyriljames@gmail.com>
2025-05-09 11:53:43 +0800

    change google callback app url from next public

--- app/api/auth/google/route.ts
+++ app/api/auth/google/route.ts
@@ -10,7 +10,7 @@ export async function GET() {
   const { data, error } = await supabase.auth.signInWithOAuth({
     provider: "google",
     options: {
-      redirectTo: `${process.env.NEXT_PUBLIC_APP_URL}/api/auth/callback?next=${routes.dashboard}`,
+      redirectTo: `${process.env.APP_URL}/api/auth/callback?next=${routes.dashboard}`,
     },
   });
 
